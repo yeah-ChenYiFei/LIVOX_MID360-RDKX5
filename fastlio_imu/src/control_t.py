7f64ef87a9bd8aaefa8fbcb315a91014e448840f
yeah-ChenYiFei <chenyanfu2006@foxmail.com>
2000-01-01 08:31:28 +0800

    子文件夹转化为普通文件夹

--- fastlio_imu/src/control_t.py
+++ fastlio_imu/src/control_t.py
@@ -1,62 +1,140 @@
 ﻿import time
 import os
 
-# ---------- 配置 ----------
-PWM_CHIP = 0       # PWM0（需确认29引脚对应PWM0）
-PWM_CHANNEL = 0    # 通道0
-PERIOD = 20000     # 周期 20ms = 50Hz（单位：微秒）
-MIN_PW = 750      # 1.0ms → -60°（左极限）
-MAX_PW = 2250      # 2.0ms → +60°（右极限）
+PWM_CHIP = 0
+PWM_CHANNEL = 0
+PERIOD_NS = 20000000
 
-# ---------- 初始化 PWM ----------
+# ==========================================
+# ⚠️ 核心配置区（必须根据你的实际情况修改！）
+# ==========================================
+STOP_PW_US = 1900      # 之前校准出来的停止点
+
+# --- 速度配置 ---
+OPEN_PW_US = 2000      # 打开方向（正转），数值越大越快
+CLOSE_PW_US = 1820     # 关闭方向（反转），数值越小越快
+HOME_PW_US  = 1850     # 归位速度（慢速反转），介于STOP和CLOSE之间，慢一点撞击更安全
+
+# --- 时间配置（核心！如果行程不一致，就改这两个时间！）---
+OPEN_TIME  = 1.0       # 打开需要转多久（秒），自己实测调整
+CLOSE_TIME = 1.0       # 关闭需要转多久（秒），通常关闭因摩擦力需要稍微长一点
+HOME_TIME  = 2.0       # 归位最长等待时间（秒），保证能撞到限位即可
+# ==========================================
+
 def pwm_init(chip, channel):
     base = f"/sys/class/pwm/pwmchip{chip}/pwm{channel}"
-    # 导出通道
+    export_path = f"/sys/class/pwm/pwmchip{chip}/export"
+    unexport_path = f"/sys/class/pwm/pwmchip{chip}/unexport"
+    
+    # ✅ 第1步：先清理残留（无视报错），确保干净状态
+    if os.path.exists(base):
+        try:
+            with open(unexport_path, "w") as f:
+                f.write(str(channel))
+        except:
+            pass
+        time.sleep(0.3)
+
+    # ✅ 第2步：重新导出 PWM 通道
+    try:
+        with open(export_path, "w") as f:
+            f.write(str(channel))
+    except:
+        pass  # 可能已经导出了，无视报错
+    time.sleep(0.5)
+
+    # ✅ 第3步：必须先禁用！！！（period=0时不能操作其他参数）
+    try:
+        with open(base + "/enable", "w") as f:
+            f.write("0")
+    except:
+        pass
+    time.sleep(0.1)
+
+    # ✅ 第4步：设置周期（必须在 duty_cycle 之前！）
+    with open(base + "/period", "w") as f:
+        f.write(str(PERIOD_NS))
+    time.sleep(0.1)
+
+    # ✅ 第5步：设置占空比为0（不能等于period！否则某些驱动报错）
+    with open(base + "/duty_cycle", "w") as f:
+        f.write("0")
+    time.sleep(0.1)
+
+    # ✅ 第6步：现在才真正启用
+    with open(base + "/enable", "w") as f:
+        f.write("1")
+    
+    print("✅ PWM 初始化成功")
+
+    base = f"/sys/class/pwm/pwmchip{chip}/pwm{channel}"
     if not os.path.exists(base):
         with open(f"/sys/class/pwm/pwmchip{chip}/export", "w") as f:
             f.write(str(channel))
-    # 设置周期
-    with open(base + "/period", "w") as f:
-        f.write(str(PERIOD))
-    # 先关输出
+        time.sleep(0.5)
     with open(base + "/enable", "w") as f:
         f.write("0")
-
-# ---------- 设置角度（适配MG996R） ----------
-def servo_set_angle(chip, channel, angle):
-    # 将代码中的angle（0~180）映射到舵机有效角度（-60~+60）
-    servo_angle = (angle / 180) * 120 - 60  # 0→-60°, 90→0°, 180→+60°
-    # 限幅到舵机有效范围
-    servo_angle = max(-60, min(60, servo_angle))
-    # 映射到脉宽（1.0ms~2.0ms）
-    pw = MIN_PW + (MAX_PW - MIN_PW) * (servo_angle + 60) / 120
-    base = f"/sys/class/pwm/pwmchip{chip}/pwm{channel}"
-    # 设置脉宽
+    with open(base + "/period", "w") as f:
+        f.write(str(PERIOD_NS))
     with open(base + "/duty_cycle", "w") as f:
-        f.write(str(int(pw)))
-    # 开启输出
+        f.write(str(PERIOD_NS))
+
+def set_pulse_width(us):
+    ns = us * 1000
+    base = f"/sys/class/pwm/pwmchip{PWM_CHIP}/pwm{PWM_CHANNEL}"
+    with open(base + "/duty_cycle", "w") as f:
+        f.write(str(ns))
     with open(base + "/enable", "w") as f:
         f.write("1")
 
-# ---------- 主循环 ----------
+def pwm_off():
+    base = f"/sys/class/pwm/pwmchip{PWM_CHIP}/pwm{PWM_CHANNEL}"
+    with open(base + "/enable", "w") as f:
+        f.write("0")
+
+def airdrop_homing():
+    """上电归位：寻找机械零点（关闭位置）"""
+    print("\n🚀 正在归位（寻找关闭限位）...")
+    print("舵机将慢速转向关闭方向，直到撞到挡块。")
+    set_pulse_width(CLOSE_PW_US)  # 慢速反转
+    time.sleep(HOME_TIME)        # 让它转足够久，确保撞到限位并死磕一会
+    set_pulse_width(STOP_PW_US)  # 立刻停止，消除嗡嗡声
+    print("✅ 归位完成！当前位置：关闭（初始位）")
+
+def airdrop_open():
+    print(f"→ 正在打开（转{OPEN_TIME}秒）...")
+    set_pulse_width(OPEN_PW_US)
+    time.sleep(OPEN_TIME)
+    set_pulse_width(STOP_PW_US)
+    print("✅ 打开完毕")
+
+def airdrop_close():
+    print(f"→ 正在关闭（转{CLOSE_TIME}秒）...")
+    set_pulse_width(CLOSE_PW_US)
+    time.sleep(CLOSE_TIME)
+    set_pulse_width(STOP_PW_US)
+    print("✅ 关闭完毕")
+
 if __name__ == "__main__":
     pwm_init(PWM_CHIP, PWM_CHANNEL)
+    time.sleep(0.5)  
+    
+    set_pulse_width(STOP_PW_US)
+    time.sleep(0.5)  # 再等一下，让硬件稳定 
+    # ⚠️ 每次上电，必须先归位！
+    airdrop_homing()
+    print("\n空投器准备就绪！")
     try:
         while True:
-            print("-60°（左极限）")
-            servo_set_angle(PWM_CHIP, PWM_CHANNEL, 0)   # 代码angle=0 → 舵机-60°
-            time.sleep(2)
-
-            print("0°（中心）")
-            servo_set_angle(PWM_CHIP, PWM_CHANNEL, 90)  # 代码angle=90 → 舵机0°
-            time.sleep(2)
-
-            print("+60°（右极限）")
-            servo_set_angle(PWM_CHIP, PWM_CHANNEL, 180) # 代码angle=180 → 舵机+60°
-            time.sleep(2)
+            cmd = input("输入指令：1=打开 | 2=关闭 | q=退出: ").strip().lower()
+            if cmd == '1': 
+                airdrop_open()
+            elif cmd == '2': 
+                airdrop_close()
+            elif cmd == 'q': 
+                break
     except KeyboardInterrupt:
-        # 退出时关闭 PWM
-        base = f"/sys/class/pwm/pwmchip{PWM_CHIP}/pwm{PWM_CHANNEL}"
-        with open(base + "/enable", "w") as f:
-            f.write("0")
-        print("\n退出，PWM 已关闭")
+        pass
+    finally:
+        pwm_off()
+        print("\n退出，PWM已关闭")
---
+++ fastlio_imu/src/test.py
@@ -0,0 +1,93 @@
+﻿import time
+import os
+
+PWM_CHIP = 0
+PWM_CHANNEL = 0
+PERIOD_NS = 20000000
+
+def pwm_init(chip, channel):
+    base = f"/sys/class/pwm/pwmchip{chip}/pwm{channel}"
+    if not os.path.exists(base):
+        with open(f"/sys/class/pwm/pwmchip{chip}/export", "w") as f:
+            f.write(str(channel))
+        time.sleep(0.5)
+    with open(base + "/enable", "w") as f:
+        f.write("0")
+    with open(base + "/period", "w") as f:
+        f.write(str(PERIOD_NS))
+    with open(base + "/duty_cycle", "w") as f:
+        f.write(str(PERIOD_NS))
+
+def set_pulse_width(us):
+    ns = us * 1000
+    base = f"/sys/class/pwm/pwmchip{PWM_CHIP}/pwm{PWM_CHANNEL}"
+    with open(base + "/duty_cycle", "w") as f:
+        f.write(str(ns))
+    with open(base + "/enable", "w") as f:
+        f.write("1")
+
+def pwm_off():
+    base = f"/sys/class/pwm/pwmchip{PWM_CHIP}/pwm{PWM_CHANNEL}"
+    with open(base + "/enable", "w") as f:
+        f.write("0")
+
+def calibrate_360():
+    print("\n" + "="*50)
+    print("🛠️ 360度舵机 停止点校准 (粗调+细调)")
+    print("="*50)
+    print("目标：找到让舵机【完全静止不动】的脉宽值")
+    print("如果它在慢转/摆动，说明还没找到停止点！\n")
+    
+    current_pw = 1500  # 从1500开始
+    
+    # ---------- 第一阶段：粗调 (步进20us) ----------
+    print("【第一阶段：粗调】(每次变化20us)")
+    print("观察舵机：如果在转，就输入相反方向让它慢下来！\n")
+    
+    while True:
+        set_pulse_width(current_pw)
+        time.sleep(0.3)
+        print(f"当前脉宽: {current_pw}us - 舵机状态？")
+        print("  W/↑: 还在正转(变大) | S/↓: 还在反转(变小) | D: 停下不动了！ | Q: 退出")
+        ans = input("请选择: ").strip().lower()
+        
+        if ans in ['w', 'up']:
+            current_pw += 20  # 粗调加20
+        elif ans in ['s', 'down']:
+            current_pw -= 20  # 粗调减20
+        elif ans == 'd':
+            print(f"\n✅ 粗调完成！大致停止点在 {current_pw}us 附近")
+            break
+        elif ans == 'q':
+            pwm_off()
+            exit()
+
+    # ---------- 第二阶段：细调 (步进2us) ----------
+    print("\n【第二阶段：细调】(每次变化2us)")
+    print("舵机现在可能还有轻微抖动或极慢爬行，微调到彻底锁死！\n")
+    
+    while True:
+        set_pulse_width(current_pw)
+        time.sleep(0.3)
+        print(f"当前脉宽: {current_pw}us - 舵机状态？")
+        print("  W/↑: 微微正转(加2) | S/↓: 微微反转(减2) | D: 完美锁死不动！ | Q: 退出")
+        ans = input("请选择: ").strip().lower()
+        
+        if ans in ['w', 'up']:
+            current_pw += 2  # 细调加2
+        elif ans in ['s', 'down']:
+            current_pw -= 2  # 细调减2
+        elif ans == 'd':
+            print("\n" + "="*50)
+            print(f"🎉🎉🎉 校准成功！精准停止点为：{current_pw}us 🎉🎉🎉")
+            print("="*50)
+            print(f"👉 请把代码开头的 STOP_PW_US 修改为 {current_pw}\n")
+            break
+        elif ans == 'q':
+            break
+            
+    pwm_off()
+
+if __name__ == "__main__":
+    pwm_init(PWM_CHIP, PWM_CHANNEL)
+    calibrate_360()
